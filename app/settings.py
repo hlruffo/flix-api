2186d1dafab91f9fa2a9bcdd0b88c354b5770a67
Hugo Ledertheil Ruffo <hugo.ruffo@extreme.digital>
2024-10-18 13:40:43 -0300

    feat: autenticação

--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'actors',
     'movies',
     'reviews',
+    'authentication',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        )
+}
